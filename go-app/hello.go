482f92f3a4d1f909063d6e97d971cf7d380701e7
Akim Gubanov <6378353+mutan@users.noreply.github.com>
2021-07-14 03:01:46 +0300

    win

--- go-app/hello.go
+++ go-app/hello.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	//response, _ := http.Get("https://iss.moex.com/iss/engines/stock/markets/bonds/boards/TQCB/securities.xml")
+	//bytes, _ := ioutil.ReadAll(response.Body)
+
+	//fmt.Println(string(bytes))
+
+	data := []byte(`<document>
+    <data id="securities">
+        <rows>
+            <row SECID="RU000A0JNYN1" BOARDID="TQCB" SHORTNAME="МГор48-об" />
+        </rows>
+    </data>
+</document>`)
+
+	var res Document
+	err := xml.Unmarshal(data, &res)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", res)
+	fmt.Printf("%#v\n", res)
+}
+
+type Document struct {
+	XMLName xml.Name `xml:"document"`
+	Data    []Data   `xml:"data"`
+}
+
+type Data struct {
+	Id   string `xml:"id,attr"`
+	Rows []Rows `xml:"rows"`
+}
+
+type Rows struct {
+	Row []Row `xml:"row"`
+}
+
+type Row struct {
+	Secid     string `xml:"SECID,attr"`
+	BOARDID   string `xml:"BOARDID,attr"`
+	SHORTNAME string `xml:"SHORTNAME,attr"`
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 ### Запускаем контейнер с компилятором Go и сразу билдим
 docker container run --rm -it -w /go/src/app -v "$PWD":/go/src/app --name test-go golang:1.16.6-buster go build -v
 
-docker-compose -f docker/docker-compose.yml run golang go run hello.go
+docker-compose run golang go run hello.go
